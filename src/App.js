3d6116ac9910ebbdba76375b185ad3952e43cceb
khoinguyen09092k <nguyendangkhoi0909@gmail.com>
2022-10-24 11:58:03 +0700

    up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+import Routers from './routers/Routers'
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
