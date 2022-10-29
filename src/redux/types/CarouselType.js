1b7795c7916b07a972e9b9765e6c5cc87fd1e915
khoinguyen09092k <nguyendangkhoi0909@gmail.com>
2022-10-29 20:37:59 +0700

    dele

--- src/redux/types/CarouselType.js
+++
@@ -1,2 +0,0 @@
-export const SET_CAROUSEL = 'SET_CAROUSEL';
-export const TOKEN = 'accessToken';
--- src/utils/setting/Config.js
+++
@@ -1,2 +0,0 @@
-export const DOMAIN = 'http://movienew.cybersoft.edu.vn';
-export const TOKEN = 'accessToken';
