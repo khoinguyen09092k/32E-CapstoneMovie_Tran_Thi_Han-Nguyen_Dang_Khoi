d185a60ece522defbbe258777ecbee5fc21350e7
khoinguyen09092k <nguyendangkhoi0909@gmail.com>
2022-11-01 23:33:42 +0700

    upp

---
+++ src/util/settings/config.js
@@ -0,0 +1,8 @@
+//Định nghĩa các tham số cố định
+// export const DOMAIN = 'https://localhost:5001';
+export const DOMAIN = 'http://movienew.cybersoft.edu.vn/api/'
+export const TOKEN = 'accessToken'
+export const GROUPID = 'GP08'
+
+
+export const USER_LOGIN = 'USER_LOGIN'
